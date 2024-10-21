d04266ca4709d1192a5ae49d8e08dac751093203
ayush5398tyagi <ksvcem5398@gmail.com>
2024-10-21 20:48:37 +0530

    enhanced the elastich search file

--- pythonProject1/elastic_access.py
+++ pythonProject1/elastic_access.py
@@ -1,11 +1,18 @@
 import time
-dict = {}
-list1 = {12,43,56}
-list2 = {"ag", "ds", "we"}
 
-for i,j in zip(list1, list2):
-    dict[i] = j
+class ElasticAccess:
+    def __init__(self):
+        self.dict = {}
+        self.list1 = {12,43,56}
+        self.list2 = {"ag", "ds", "we"}
 
-for i in range(10):
-    print(dict)
-    time.sleep(2)
+    def check_querry_response(self):
+        for i,j in zip(self.list1, self.list2):
+            self.dict[i] = j
+
+        for i in range(10):
+            print(self.dict)
+            time.sleep(2)
+
+object_of_ElasticAccess = ElasticAccess()
+object_of_ElasticAccess.check_querry_response()
